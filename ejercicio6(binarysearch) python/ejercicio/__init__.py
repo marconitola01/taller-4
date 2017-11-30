ec3ce32d34e303380622a1fa523fd45777d12b6a
marco nitola <33072491+marconitola01@users.noreply.github.com>
2017-11-29 21:05:34 -0500

    Add files via upload

---
+++ ejercicio6(binarysearch) python/ejercicio/__init__.py
@@ -0,0 +1,54 @@
+from random import randint
+from builtins import str
+
+
+def Quick_sort(V):
+    pivote = len(V)//2
+    menor, igual, mayor = [],[],[]
+    
+    for i in range(len(V)):
+        if(V[i] < V[pivote]): menor.append(V[i])
+        elif(V[i]> V[pivote]): mayor.append(V[i])
+        else: igual.append(V[i])
+        
+    if len(menor)>1: 
+        menor = Quick_sort(menor)
+    if len(mayor)>1: 
+        mayor = Quick_sort(mayor)
+    return menor + igual + mayor
+
+def main():
+    
+    Tamain = (input("Escriba la Sucesion de numeros separados por comas \n"))
+    print(Tamain)
+    Tamain_2 = Tamain.split(",")
+    print(Tamain_2)
+    Tamain_3 = Quick_sort(Tamain_2)
+    print(Tamain_3)
+    
+    Valor = (input("Ingrese un numero a buscar \n"))
+    
+    Inicio = 0
+    Final = (len(Tamain_3)-1)
+    Encontro = False
+    
+    while(not(Encontro) and Inicio <= Final):
+        mitad = int((Inicio + Final)/2)
+        if(Valor == Tamain_3[mitad]):
+            Encontro = True
+            Num = mitad
+        elif(Valor < (Tamain_3[mitad])):
+            Final = mitad - 1
+            
+        else:
+            Inicio = mitad + 1
+    print()
+    
+    if(Encontro):
+        print("El Numero se encuentra y se localiza en la posicion " + str(Num))
+    else:
+        print("El dato no se encuentra")
+    print()
+       
+       
+main()

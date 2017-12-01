a6c59d22729f2d65a7d64c9d7f41c63ba6bcb711
marco nitola <33072491+marconitola01@users.noreply.github.com>
2017-12-01 08:51:38 -0500

    Update __init__.py

--- ejercicio4(quicksort)/ejercicio/__init__.py
+++ ejercicio4(quicksort)/ejercicio/__init__.py
@@ -1,28 +1,28 @@
-from random import randint #import library random
+from random import randint #librerias
 
 
-def Quick_sort(V): #method quicksort
-    pivote = len(V)//2 #select pivote
-    menor, igual, mayor = [],[],[] #create array
+def Quick_sort(V): #metodo quicksort
+    pivote = len(V)//2 #seleccion de  pivote
+    menor, igual, mayor = [],[],[] #crear array
     
-    for i in range(len(V)):#conditional where add array
-        if(V[i] < V[pivote]): menor.append(V[i]) #add depending the condition
-        elif(V[i]> V[pivote]): mayor.append(V[i]) #add depending the condition
+    for i in range(len(V)): #ciclo para recorrer array
+        if(V[i] < V[pivote]): menor.append(V[i]) #añadimos dependiendo la condicion
+        elif(V[i]> V[pivote]): mayor.append(V[i]) 
         else: igual.append(V[i])
         
     if len(menor)>1: 
-        menor = Quick_sort(menor) #organize
+        menor = Quick_sort(menor) #organiza
     if len(mayor)>1: #organize
         mayor = Quick_sort(mayor)
-    return menor + igual + mayor #return array organized
+    return menor + igual + mayor #retornamos array organizado
 
 
 def main(): #method main
     
-    Tamain = (input("Escriba la Sucesion de numeros separados por comas \n")) #ask value
-    print(Tamain)
-    Tamain_2 = Tamain.split("," or ".")
-    print(Tamain_2)
-    print(Quick_sort(Tamain_2))
+    elementos = (input("Escriba la Sucesion de numeros separados por comas \n")) #pedimos los datos
+    
+    elementos_2 = elementos.split("," or ".") #usamos el split
+ 
+    print(Quick_sort(elementos_2))
     
 main()

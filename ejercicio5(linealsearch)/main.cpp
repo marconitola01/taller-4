543b9b748cb51ac0ae8351bf7e49c77edd85c7b0
marco nitola <33072491+marconitola01@users.noreply.github.com>
2017-12-01 07:55:36 -0500

    Update main.cpp

--- ejercicio5(linealsearch)/main.cpp
+++ ejercicio5(linealsearch)/main.cpp
@@ -1,34 +1,63 @@
-#include<iostream>
-#include "conio.h"
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
 
-using namespace std;
-int main(){
-	int numero;
-	int i=0;
-	int pos;
-	int n;
-	bool encontro = false;
-	cout<<"ingrese la cantidad de numeros";cin>>n;
-	int vec[n];
-	for(int i=0;i<n;i++){
-		cout<<i+1<<" -> ";
-            cin>>vec[i];
-            cout<<endl;
+ using namespace std;
+
+ int busquedalineal(int array[],int tam, int valor){  //metodo busqueda lineal
+ 	for(int i = 0 ; i < tam ; i++){                  
+ 		if(valor == array [i]){
+		 return i ;
+		 }
+	 }
+	return -1;
+ }
+  int arreglo(int array[], int tam){
+  	for(int i = 0 ; i < tam ; i++){
+ 		cout <<" "<<array[i]<<" \n";
+	 }
+  }
+ 
+ int main (){  //main para correr programa
+ 	
+ 	int x;
+ 	int y;
+ 	
+ 	string str;
+    vector<int> vect;
+    
+    cout <<"ingrese el tamaño del arreglo: " << endl;
+ 	cin >> x;
+    int a[x];
+    cout << "Digite los numeros separados por coma: \n";
+    cin >> str;
+    stringstream ss(str);
+    int i;
+    
+    while (ss >> i){
+        vect.push_back(i);
+        if (ss.peek() == ',')
+            ss.ignore();
+    }
+    for (i=0; i< vect.size(); i++){
+    	a[i] = vect.at(i);
 	}
-	cout<<"ingrese el numero que se busca";
-	cin>>numero;
+        
+ 	
+ 	
+ 	cout <<"ingrese numero que desea buscar: " << endl;
+ 	cin >> y;
+
+	int result = busquedalineal(a,x,y);
+		
+	arreglo(a,x);
 	
-	while(!(encontro)&& i<n){
-		if(numero == vec[i]){
-			encontro = true;
-			pos = i;
-			
-		}
-	i=i+1;
-	}
-	if(encontro){
-		cout<<"el dato esta en la posicion"<<pos+1;
-	
-	}
-	_getch();
-}
+ 	if(result >= 0){
+ 		cout << "El numero " << a[result]<< " se encontro"
+		 " en la posicion " << result << endl;
+	 }
+	 else{
+	 	cout << "El numero " << y <<" no se encontro. "<< endl;
+	 }
+ }

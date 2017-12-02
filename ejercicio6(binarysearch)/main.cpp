025f0e16022152b3c2d779e057384bd29763377b
marco nitola <33072491+marconitola01@users.noreply.github.com>
2017-12-02 11:16:50 -0500

    Update main.cpp

--- ejercicio6(binarysearch)/main.cpp
+++ ejercicio6(binarysearch)/main.cpp
@@ -1,33 +1,33 @@
 #include<iostream>
-#include"conio.h"
+#include"conio.h"  /*librerias*/
 
 using namespace std;
 
-int main(){
+int main(){   /*main to run our program*/
 	int numero;
 	int puntero;
 	int final;
-	int mitad;
+	int mitad; 
 	bool encontro = false;
-	int n;
-	cout<<"ingrese la cantidad de numeros";
-	cin>>n;
+	int n;                           /*variables to use*/
+	cout<<"ingrese la cantidad de numeros"; /* we ask the size of the array*/
+	cin>>n;                                 /* n is the size of array*/
 	int vec[n];
-	for(int i=0;i<n;i++){
-		cout<<i+1<<" -> ";
+	for(int i=0;i<n;i++){                               /*cycle for*/ 
+		cout<<i+1<<" -> ";                           /*ask the values*/   
             cin>>vec[i];
             cout<<endl;
 	}
-	cout<<"ingrese el numero a buscar ";
-	cin>>numero;
+	cout<<"ingrese el numero a buscar ";            /* we ask the number to work*/
+	cin>>numero;                                    /* we assign the variable numero*/
 	
-	while(!encontro&&puntero<=final){
+	while(!encontro&&puntero<=final){                 /* while cycle*/
 		mitad = (int)((puntero+final)/2);
 		
-		if(numero == vec[mitad])
+		if(numero == vec[mitad])                 
 			encontro = true;
 		
-	else if(numero<vec[mitad]) 
+	else if(numero<vec[mitad])          
 	final=mitad-1;
 	else
 	  puntero=mitad+1;

9bb7c7ce10abc0b40294bcd3f9a465ea49fe700b
marco nitola <33072491+marconitola01@users.noreply.github.com>
2017-12-02 11:02:26 -0500

    Update main.cpp

--- ejerciciouno(bubblesort)/main.cpp
+++ ejerciciouno(bubblesort)/main.cpp
@@ -6,22 +6,22 @@
 #include <iostream> /*librerias*/
 #include <conio.h>
 using namespace std;
-int main() /*main para correr nuestro programa*/
+int main() /*main to run our program*/
 {
     string str;
     vector<int> vect;
-    int i, j , temp; /*variables a usar*/
-    int n;/* creamos la variable n para que guarde el tamaño del arreglo*/
-    cout<<"ingrese cuantos numeros va a ordenar";cin>>n; /*pedimos por teclado el tamaño del arreglo*/
-	int vector[n]; /*definimos que n sera el tamaño del arreglo*/
+    int i, j , temp; /*variables to use*/
+    int n;/* we create an variable n for keep the size*/
+    cout<<"ingrese cuantos numeros va a ordenar";cin>>n; /*we ask for keyboard the size of the array*/
+	int vector[n]; /*n will be the size of the array*/
 	
-    cout << "Digite los numeros separados por coma: \n";
+    cout << "Digite los numeros separados por coma: \n"; /*ask the values*/
     cin >> str;
     stringstream ss(str);
     
     while (ss >> i){
         vect.push_back(i);
-        if (ss.peek() == ',')
+        if (ss.peek() == ',')  /*"split" to separate the numbers */
             ss.ignore();
     }
     for (i=0; i< vect.size(); i++){
@@ -29,24 +29,24 @@ int main() /*main para correr nuestro programa*/
 	}
 	
 	 
-    for(i=0;i<n;i++) // ciclo para recorrer el arreglo 
+    for(i=0;i<n;i++) // cycle for  
     {
-        for(j=i+1;j<n;j++)//cilco para recorre el arreglo (j inicia en uno por que nos interesa comparar una posicion mas arriba que la de i para que sean parejas). 
+        for(j=i+1;j<n;j++)//cycle to go through the arrangement (j starts in one because we want to compare a position higher than that of i to be pairs) 
         {
-            if(vector[j]<vector[i])  //comparamos los elementos en sus respectivas posiciones sea j menor que i(se comparan parejas adyacentes).
+            if(vector[j]<vector[i])  //we compare the elements in their respective positions if j is less than i (adjacent pairs are compared)
             {
                 temp=vector[j]; 
-                vector[j]=vector[i]; // ordenamos los numeros menores en las posiciones iniciales y guardamos
-                vector[i]=vector[j];  // los mayores se iran a las posiciones finales
+                vector[j]=vector[i]; // we order the minor numbers in the initial positions and keep
+                vector[i]=vector[j];  // the biggest numbers will go to the final positions
                 vector[i]=temp; 
             }
         }
     }
     for(i=0;i<n;i++)
     {
-        cout<<vector[i]<<" "; //imprimimos el arreglo ya ordenado
+        cout<<vector[i]<<" "; //we print the array already ordered
     }
-    system("pause"); // lo uso para que el programa no se cierre apenas terminar //
+    system("pause"); // I use it so that the program does not close just after //
 	return 0;
     
 	}

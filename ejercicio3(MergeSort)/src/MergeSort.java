2d7fa745989efc438bdd7bc708fe05e785c80806
marco nitola <33072491+marconitola01@users.noreply.github.com>
2017-12-02 10:54:13 -0500

    Update MergeSort.java

--- ejercicio3(MergeSort)/src/MergeSort.java
+++ ejercicio3(MergeSort)/src/MergeSort.java
@@ -2,38 +2,37 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class MergeSort
 {
-	public static void main(String[] args) //main para correr nuestro programa
+	public static void main(String[] args) //mmain to run our program
 	{
-		int n;   // creamos la variable n para que sea eñ tamaño deñ arreglo
-		Scanner in=new Scanner(System.in); // creamos un objeto para guardar el dato
-		  System.out.println("ingrese la cantidad de numeros"); // pedimos el dato
-                n=in.nextInt();    // guardamos el dato en n
-		int A []=new int[n],i;  // creamos un arreglo que sera de tamaño n
-		for(i=0;i<n;++i)       // ciclo que recorrera el arreglo y lo llenara pidiendo datos desde teclado
-		{
-		   System.out.println("ingrese numero");	//pedimos los numeros
+		int n;     // we create the variable n to be the size of the array
+		Scanner in=new Scanner(System.in);         // create an object to keep the date
+		  System.out.println("ingrese la cantidad de numeros"); // ask the value
+                n=in.nextInt();    // we keep the value in n
+		int A []=new int[n],i;  // we create an array, which size is n
+		for(i=0;i<n;++i)       // cycle that travels the array and fill it asking for data from keyboard
+		   System.out.println("ingrese numero");	//ask value
                     A[i]=in.nextInt();                          // a cada posicion le asignamos un numero 
 		
 		}
-		System.out.println("lista original"+"\n"+Arrays.toString(A));  // imprimimos la lista original
-                A=MergeSort(A);         // invocamos el metodo MergeSort que va a ordenar 
-		System.out.println();   // para imprimir un espacio 
-		for(i=0;i<n;++i)        // cicllo que recorre el arreglo y lo imprime (arreglo ya ordenado)
+		System.out.println("lista original"+"\n"+Arrays.toString(A));  // we print the original list
+                A=MergeSort(A);         // we call the MergeSort method that will order 
+		System.out.println();   // print an space 
+		for(i=0;i<n;++i)        // cicle for print the original array
 		{
 			System.out.print(A[i]+" ");
 		}
 	}
-	public static int[] MergeSort(int [] A)   //metodo que nos ordenara el arreglo
+	public static int[] MergeSort(int [] A)   //ordering method
 	{
-		if(A.length==1)          // si el arreglo solo tiene una posicion ya esta ordenado entonces retornamos el arreglo tal cual
+		if(A.length==1)          // if the array only has one position, is already ordered, then we return the array
 		return A;
-                else                     // si tiene mas posiciones 
+                else                     // 
 		{
-			int n=(int) Math.floor(A.length/2);  // n sera la mitad del arreglo (para separar mitades)
-			int B []=new int [n];                // creamos un arreglo auxiliar B y de tamaño sera n osea la mtad del arreglo A 
-			int C []=new int [A.length-n];       // otro arerglo este sera C y tendra de tamaño la mitad de A  
+			int n=(int) Math.floor(A.length/2); //n will be half of the array
+			int B []=new int [n];                // we create an auxiliar array, which size is n 
+			int C []=new int [A.length-n];       // other array C this has size half A  
 			int i;
-			for(i=0;i<A.length;++i)    // ciclo para recorrer arreglo
+			for(i=0;i<A.length;++i)    // cycle for
 			{
 				if(i<n)            // si i es menor que n intercambiamos elementos de A para que queden guardados en B y asi ordenar 
 					B[i]=A[i];
@@ -46,7 +45,7 @@ public class MergeSort
 			return A;
 		}
 	}
-	public static int [] Merge(int [] A,int [] B) // metodo para ordenar arreglos ya separados
+	public static int [] Merge(int [] A,int [] B) //method for ordering separate arrangements
 	{
 		int C []=new int [A.length+B.length]; 
 		int i,a=0,b=0;
